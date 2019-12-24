26b8ca240acf6fb79e2488a3a97aabd926ec8e00
rResin <a845613099@163.com>
2019-12-24 17:08:19 +0800

    update

--- miniprogram/pages/cloud/cloud.js
+++ miniprogram/pages/cloud/cloud.js
@@ -1,4 +1,5 @@
 // pages/cloud/cloud.js
+const db = wx.cloud.database();///  初始化数据库
 Page({
 
   /**
@@ -8,6 +9,64 @@ Page({
 
   },
 
+  insert : function (){
+    /* db.collection('user').add({
+      data:{
+        name :'jerry',
+        age : '20'
+      },
+      success:res=>{
+        console.log(res);
+      },
+      fail :err =>{
+        console.log(err)
+      }
+    }) */
+    db.collection('user').add({
+      data: {
+        name: 'jack',
+        age: 21
+      }
+    }).then(res =>{
+      console.log(res)
+      }).catch(err =>{
+      console.log(err)
+      })
+  },
+  update: function () {
+    db.collection('user').doc('dbff9fc75e01bf11067f19f53213b5c6').update({
+      data: {
+        name: 'jerry111',
+        age: 20
+      }
+    }).then(res => {
+      console.log(res)
+    }).catch(err => {
+      console.log(err)
+    })
+  },
+  search: function () {
+    db.collection('user').where({
+     name: 'jack'
+    }).get().then(res => {
+      console.log(res)
+    }).catch(err => {
+      console.log(err)
+    })
+  },
+  sum:function(){
+    wx.cloud.callFunction({
+      name :'sum',
+      data :{
+        a: 2,
+        b:3
+      }
+    }).then(res =>{
+      console.log(res)
+      }).catch(err =>{
+        console.log(err)
+        })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
